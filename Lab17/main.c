dc4b5650640a33f040d398e103af0d8b0f7411e8
KostyaBIST221 <ksapov24@mail.ru>
2022-12-08 10:41:45 +0300

    Initial Commit

---
+++ Lab17/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Lab17
+//
+//  Created by kostya on 08.12.2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
